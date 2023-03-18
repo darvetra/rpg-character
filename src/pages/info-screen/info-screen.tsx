48ab51e1df830d18097be3f435b9eb5daf75d69a
DarVetra <darvetra13@gmail.com>
2023-03-18 16:14:07 +0300

    добавляет стили в компонент Info

--- src/pages/info-screen/info-screen.tsx
+++ src/pages/info-screen/info-screen.tsx
@@ -1,10 +1,12 @@
 import Header from '../../components/header/header';
-import Character from '../../components/character/character';
-import Inventory from '../../components/inventory/inventory';
 import Footer from '../../components/footer/footer';
 
-import styles from './info-screen.module.scss';
+// import Character from '../../components/character/character';
+// import Inventory from '../../components/inventory/inventory';
+// import Parameters from "../../components/parameters/parameters";
+// import Equipment from "../../components/equipment/equipment";
 
+import styles from './info-screen.module.scss';
 
 function InfoScreen(): JSX.Element {
 
@@ -14,8 +16,90 @@ function InfoScreen(): JSX.Element {
 
       <main className={styles['main-page']}>
         <h1 className="visually-hidden">Rotgar.Guide</h1>
-        <Character />
-        <Inventory />
+
+
+        {/*<Character />*/}
+        <section className={styles['character']}>
+          <div className={styles['character__container']}>
+            <div className={styles['character__block']}>
+              {/*<Parameters />*/}
+
+
+              {/*<Equipment />*/}
+              <section className={styles['character__item']}>
+                <section className={styles['equipment']}>
+                  <h2 className={styles['equipment__title']}>Экипировка персонажа</h2>
+
+                  <div className={styles['equipment__container']}>
+                    <ul className={styles['equipment__column']}>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                        <picture>
+                          <source
+                            type="image/webp"
+                            width="86"
+                            height="86"
+                            srcSet={require('../../img/sword.webp')}
+                          />
+                          <img
+                            alt="img"
+                            width="86"
+                            height="86"
+                            src={require('../../img/sword.png')}
+                          />
+                        </picture>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                    </ul>
+
+                    <ul className={styles['equipment__column']}>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                        <picture>
+                          <source
+                            type="image/webp"
+                            width="86"
+                            height="86"
+                            srcSet={require('../../img/shield.webp')}
+                          />
+                          <img
+                            alt="img"
+                            width="86"
+                            height="86"
+                            src={require('../../img/shield.png')}
+                          />
+                        </picture>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                      <li className={styles['equipment__slot']}>
+                      </li>
+                    </ul>
+                  </div>
+                </section>
+
+              </section>
+
+
+            </div>
+          </div>
+        </section>
+
+
+        {/*<Inventory />*/}
       </main>
 
       <Footer />
